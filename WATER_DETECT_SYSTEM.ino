a79772d54a0a0baa97829d0379e82078cd914239
T.Yoshino <takafumi_yoshino@vacancorp.com>
2017-09-26 03:36:05 +0900

    show the status on the LCD

--- WATER_DETECT_SYSTEM.ino
+++ WATER_DETECT_SYSTEM.ino
@@ -3,6 +3,7 @@
 // PCAL9555APW Arduino Sample
 *******************************************************************************/
 #include <Wire.h>
+#include <ST7032.h>
 
 #define PCAL9555APW_ADDR 0x20 // in 7bit for Arduino
 
@@ -30,6 +31,10 @@
 #define LEDC_TIMER_BIT 13
 #define LEDC_BASE_FREQ 5000
 
+//LCD
+ST7032 lcd;
+bool isLcdOn = false;
+
 void datasend(int,int,int *,int);
 void dataread(int,int,int *,int);
 
@@ -41,13 +46,18 @@ void setup()
   // BUZZER
   ledcSetup(LEDC_CHANNEL, LEDC_BASE_FREQ, LEDC_TIMER_BIT);
   ledcAttachPin(BUZZER_PIN, LEDC_CHANNEL);
-  
+
+  //LCD
+  lcd.begin(8, 2);
+  lcd.setContrast(30);            // コントラスト設定
+  lcd.clear();
+
 }
 
 void loop()
 {
   boolean isWaterDetect = false;
-  String strDetectedSensors = "Sensor : "; 
+  String strDetectedSensors = " No"; 
 
   int port_input[2]; //入力状態を取得する変数
   dataread(PCAL9555APW_ADDR, INPUT_REG, port_input, 2);  
@@ -61,7 +71,7 @@ void loop()
     Serial.println(value);
     if(value==0){
       isWaterDetect=true;
-      strDetectedSensors = strDetectedSensors + String(i) + String(",");
+      strDetectedSensors = strDetectedSensors + String(".") + String(i);
     }
   }
   Serial.print("\tport1:");
@@ -74,7 +84,13 @@ void loop()
     Serial.println(value);
     if(value==0){
       isWaterDetect=true;
-      strDetectedSensors = strDetectedSensors + String(i+8) + String(",");
+      strDetectedSensors = strDetectedSensors + String(".") + String(i+8);
+    }
+  }
+
+  if((strDetectedSensors.length()-1) < 9){
+    for (int thisChar = 0; thisChar < (9 -(strDetectedSensors.length()-1)); thisChar++) {
+      strDetectedSensors = strDetectedSensors + String(" ");
     }
   }
 
@@ -82,15 +98,69 @@ void loop()
   if(isWaterDetect){
     Serial.println("WARNING");
     Serial.println(strDetectedSensors);
+
+    ///////////
+    //LCD
+    ///////////
+    lcd.setCursor(0, 0);
+    lcd.print("WARNING!!");
+  
+    // print from 0 to 10:
+    lcd.setCursor(0, 1);
+    lcd.print(strDetectedSensors);
+
+    if(strDetectedSensors.length()>8){
+        Serial.println("LONG LENGTH");
+        Serial.println(strDetectedSensors.length());
+
+        lcd.autoscroll();
+        for (int thisChar = 0; thisChar < (strDetectedSensors.length()-8); thisChar++) {
+         lcd.setCursor(strDetectedSensors.length(), 1);
+         lcd.print(".");
+         lcd.noAutoscroll();
+         lcd.setCursor(thisChar+1, 0);
+         lcd.print("WARNING!!");
+         lcd.autoscroll();
+         delay(500);
+        }
+        lcd.noAutoscroll();
+        lcd.noDisplay();
+        lcd.setCursor(0, 1);
+        lcd.print("                                ");
+        for (int thisChar = 0; thisChar < (strDetectedSensors.length()-8); thisChar++) {
+           lcd.scrollDisplayRight();
+        }
+        lcd.setCursor(0, 0);
+        lcd.print("WARNING!!");
+      
+        // Turn on the display:
+        lcd.display();
+
+    }else{
+        Serial.println("LONG LENGTH");
+        Serial.println(strDetectedSensors.length());
+    }
+    
+    ///////////
     // buzzer start
+    ///////////
     callZeldaSound();   // ゼルダの謎解き音を鳴らす
-     
+
   }else{
+    lcd.clear();
     Serial.println("No Problem");
     Serial.println(strDetectedSensors);
+    //LCD
+      lcd.setCursor(0, 1);
+      lcd.print("StatusOK");
+      lcd.setCursor(0, 0);
+    if(isLcdOn){
+      lcd.print("Monitor.");
+    }else{
+      lcd.print("Monitor");
+    }
+    isLcdOn = !isLcdOn;
   }
-
-
 
   delay(500);
 }
